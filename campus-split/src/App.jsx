e33974598f7844b09ac7f4d154d9954194fd18ff
unknown <lovejawalia@gmail.com>
2026-09-13 18:18:48 +0530

    Update API_BASE to Render live backend

--- campus-split/src/App.jsx
+++ campus-split/src/App.jsx
@@ -25,7 +25,7 @@ import {
   ExternalLink
 } from 'lucide-react';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://campussplit-api.onrender.com';
 
 // --- INTEGRATED SVG DEBT GRAPH COMPONENT ---
 function DebtGraphView({ members, rawEdges = [], settlements = [], netBalances = {} }) {
